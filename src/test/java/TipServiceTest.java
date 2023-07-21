7106ddab2ea2276190023b0fbf5fcff90b13c727
MilaKaz <LK9261580661@yandex.ru>
2023-07-21 17:28:25 +0300

    commit exercise_2.1.13

--- src/test/java/TipServiceTest.java
+++ src/test/java/TipServiceTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class TipServiceTest {
-}
+import org.example.TipService;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    public class TipServiceTest {
+
+        @ParameterizedTest(name = "Граничные значения суммы покупки {0}")
+        @CsvSource({ "1", "500",  "999", "1000", "1001", "10000"})
+        void roundTipTest(BigDecimal amount) {
+
+            BigDecimal expectedResult;
+            if (amount.compareTo(BigDecimal.valueOf(1000)) < 0) {
+                expectedResult = amount.multiply(BigDecimal.valueOf(1.1));
+            } else {
+                expectedResult = amount.multiply(BigDecimal.valueOf(1.05));
+            }
+
+            TipService tipService = new TipService();
+            BigDecimal actualResult = tipService.roundTips(amount);
+            assertEquals(expectedResult, actualResult);
+        }
+    }
+
+
+
+
